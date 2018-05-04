047ebf666f2e8a4b84aefae7e6a44abca6fe2b9f
v6854384 <32385043+v6854384@users.noreply.github.com>
2018-05-04 13:53:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,45 +2,45 @@
 #include<locale>
 using namespace std;
 
-struct Node {
-    int data;
-    Node *left;
-    Node *right;
+struct Node { //создаем структуру
+    int data; //поле для информации
+    Node *left; //указатель на левую ветку
+    Node *right; //указатель на правую ветку
 };
 
-class BSTree {
-    private:
-        Node *root;
-    public:
-        BSTree() { root = nullptr; }
-        void add(Node *&root, int data) {
-            if (root == nullptr) {
-                root = new Node { data, nullptr, nullptr };
+class BSTree { //создаем класс дерево
+    private: //параметр с начальной информацией, который недоступен для редактирования
+        Node *root; 
+    public: //публичные данные
+        BSTree() { root = nullptr; } //объявляем переменную, тип которой структура дерево
+        void add(Node *&root, int data) { //функция записи элемента в бинароное дерево
+            if (root == nullptr) { //если дерева не существует
+                root = new Node { data, nullptr, nullptr }; //выдеояем память под первый аргумент
             }
-            if (data < root->data) {
-                if (root->left = nullptr) {
-                        root->left = new Node { data, nullptr, nullptr };
+            if (data < root->data) {  //сранвниваем элементы
+                if (root->left = nullptr) { //если меньше и  слева ничего нет
+                        root->left = new Node { data, nullptr, nullptr }; //то создаем слева элемент, имеющий текущую информацию и не имеющий разветлений
                 }
-                else add(root->left, data);
+                else add(root->left, data); //иначе с помощью рекурсии переходим на ветку ниже и заного проделываем оперцию add
             }
-            if (data > root->data) {
+            if (data > root->data) { //здесь все то же самое, если элемент больше начального
                 if (root->right = nullptr) {
                         root->right = new Node { data, nullptr, nullptr };
                 }   
                 else add(root->right, data);
             }
         }
-        void insert(int data) { add (root, data); }
-        void delete_tree(Node *&data) {
-            if (data != nullptr) {
-                delete_tree(data->left);
-                delete_tree(data->right);
+        void insert(int data) { add (root, data); } //создание дерева
+        void delete_tree(Node *&data) { //удаление дерева
+            if (data != nullptr) { //поиск не пустых узлов
+                delete_tree(data->left); //удаление всех элементов с помощью рекрсии                       
+                delete_tree(data->right); //
                 delete data;
                 data = nullptr;
             }
         }
         ~BSTree() {
-            delete_tree(root);
+            delete_tree(root); //полное удаление дерева
         }
 };
 
@@ -58,10 +58,10 @@ void menu(){
 }
 
 int main(int argc, char* argv[]) {
-    setlocale(LC_ALL, "Russian");
-    BSTree tree;
+    setlocale(LC_ALL, "Russian"); 
+    BSTree tree; //создаем пустое дерево класса дерево
     for (int i=1; i<argc; i++) { 
-        tree.add(atoi(argv[i]));
+        tree.add(atoi(argv[i])); //заполнение дерева элементами
     };
     int choice = 0;
     while (true) {
