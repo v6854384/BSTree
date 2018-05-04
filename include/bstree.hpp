517235a00bf0b3cdd39984c36bcb8bcb36e11bb3
v6854384 <32385043+v6854384@users.noreply.github.com>
2018-05-04 14:04:53 +0300

    Update bstree.hpp

--- include/bstree.hpp
+++ include/bstree.hpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+namespace BSTree {
+    struct Node {
+      int data;
+     Node *left;
+     Node *right;
+  };
+
+  class Tree {
+      private:
+          Node *root;
+      public:
+        Tree();
+        void add(Node *&root, int data);
+        void insert(int data);
+        void delete_tree(Node *&data); 
+  }
