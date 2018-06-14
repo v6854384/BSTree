a3be842802f6ef194c8238052e0a68928d0807c6
v6854384 <32385043+v6854384@users.noreply.github.com>
2018-06-14 10:32:20 +0300

    Update BSTree.hpp

--- include/BSTree.hpp
+++ include/BSTree.hpp
@@ -1,19 +1,36 @@
-#include <iostream>
-using namespace std;
+#include<iostream>
+#include<fstream>
+namespace BSTREE{
+struct Node{
+    int data;
+    Node *left;
+    Node *right;
+    Node *parent;
+};
 
-namespace BSTree {
-    struct Node {
-      int data;
-     Node *left;
-     Node *right;
-  };
+class BSTree{
+    private:
+    Node *root;
+    auto insert(Node*& root,Node*& parent,Node*& value)-> Node*;
+    void delete_tree(Node *&data);
+    void show(Node*node, int stage);
+    void direct_bypass(Node* root);
+    void cross_bypass(Node* root);
+    void reverse_bypass(Node* root);
+    void direct_bypass(Node* root, std::ofstream &fout);
+    auto delete_node(Node *&root, int value) ->bool;
+    auto insert(Node*& root, Node*& parent, int value) -> Node*;
 
-  class Tree {
-      private:
-          Node *root;
-      public:
-        Tree();
-        void add(Node *&root, int data);
-        void insert(int data);
-        void delete_tree(Node *&data); 
-  }
+
+    public:
+    BSTree();
+    void insert(int value);
+    void show();
+    void select_bypass( char choice_bypass );
+    bool upload_to_file(std::string& road);
+    bool unload_from_file(std::string& road);
+    bool exists(int value) const;
+    auto delete_node(int value) ->void;
+    ~BSTree();
+    };
+}
